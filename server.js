a3f38c7e6947f52cc7400be7c108d68169fb5488
Reymond Razote (Daimler) <fpt.razote@daimlertruck.com>
2025-08-26 15:35:29 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res) => {
+	res.status(404).render('page-not-found', { pageTitle: 'Page not found' });
+});
+
+app.listen(3000);
